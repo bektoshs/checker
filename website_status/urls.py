67fa2ea82ada18251aabf4f5b529e9855f05c7a0
Bektosh <beki.s@inbox.ru>
2024-07-19 18:05:35 +0500

    Docker with url

--- website_status/urls.py
+++ website_status/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseNotFound
+
+admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('checker/', include('checker.urls')),
+    path('secret-admin/', admin.site.urls),
+    path('secret-checker/', include('checker.urls')),
 ]
+
+
+def custom_page_not_found(request, exception=None):
+    return HttpResponseNotFound('Page not found')
+
+handler404 = 'websites_url.urls.custom_page_not_found'
